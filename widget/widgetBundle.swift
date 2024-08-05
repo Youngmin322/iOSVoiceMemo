324cf64c2e0576b57e8eda6dd53702cf3005bd4e
Youngmin322 <choyoungmin0322@gmail.com>
2024-08-05 15:16:21 +0900

    widget 기본 기능 추가

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by 조영민 on 8/5/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetLiveActivity()
+    }
+}
